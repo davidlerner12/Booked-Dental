4338f270913c0d6e1df93bd839f09c9a2cfb082a
DaniellKondoff <danielkondov91@gmail.com>
2026-03-06 14:11:58 +0200

    Who its for and who is not for

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,6 +8,8 @@ import AffordableByDesign from "@/components/sections/AffordableByDesign";
 import ROICalculator from "@/components/sections/ROICalculator";
 import WhyUGCWorks from "@/components/sections/WhyUGCWorks";
 import HowItWorks from "@/components/sections/HowItWorks";
+import WhoItsFor from "@/components/sections/WhoItsFor";
+import WhoItsNotFor from "@/components/sections/WhoItsNotFor";
 import Testimonials from "@/components/sections/Testimonials";
 import CreativesVideo from "@/components/sections/CreativesVideo";
 import MarketExclusivity from "@/components/sections/MarketExclusivity";
@@ -27,6 +29,8 @@ const Index = () => {
       <ROICalculator />
       <WhyUGCWorks />
       <HowItWorks />
+      <WhoItsFor />
+      <WhoItsNotFor />
       <Testimonials />
       <CreativesVideo />
       <MarketExclusivity />
